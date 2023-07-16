4adbc15c7fb7cd357ee8fa333396f0a985c5db5d
mins00oo <alstn7223@gmail.com>
2023-07-16 12:32:05 +0900

    swea문제 추가

--- BACKJOON/B2_13300.java
+++ BACKJOON/B2_13300.java
@@ -1,9 +1,13 @@
 package BACKJOON;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class B2_13300 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
 	}
 
