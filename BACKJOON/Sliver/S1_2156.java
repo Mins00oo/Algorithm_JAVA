7c6244c2e7d0871cf25ebe534aab2e410e102071
mins00oo <alstn7223@gmail.com>
2023-07-21 23:09:40 +0900

    2156번 백준

---
+++ BACKJOON/Sliver/S1_2156.java
@@ -0,0 +1,23 @@
+package BACKJOON.Sliver;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class S1_2156 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] wine = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			wine[i] = sc.nextInt();
+		}
+		System.out.println(Arrays.toString(wine));
+		
+		int[] dp = new int[n];
+		dp[0] = 0;
+	}
+
+}
