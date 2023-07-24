361352f03853d7751ef363aa19198fce37c77120
'mins00oo' <'alstn7223@gmail.com'>
2023-07-25 00:04:12 +0900

    알고리즘 문제 풀이 - 최소 힙 문제
    우선순위 큐는 자바에서 PriorityQueue로 선언해서 생성할 수 있고 문제에서 주어진
    조건에 맞추어서 출력을 해주면 됨

---
+++ BACKJOON/Sliver/S1_11286.java
@@ -0,0 +1,13 @@
+package BACKJOON.Sliver;
+
+import java.util.Scanner;
+
+public class S1_11286 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+	}
+
+}
