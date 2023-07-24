5a29c380fbe2f58987266a8dfab661753355fff8
'mins00oo' <'alstn7223@gmail.com'>
2023-07-25 00:22:58 +0900

    우선순위 큐 문제 - 백준_S2_1927
    앞서 푼 우선순위 큐는 최소 힙 문제라면 그와 반대로 최댓값을 우선으로 제거하기 위한 최대 힙은
    처음 큐를 선언할 때 new PriorityQueue<>(Comparator.reverseOrder());
    Comparator.reverseOrder()을 붙여주면 큰 값을 기준으로 먼저 큐에 들어간다!!

--- BACKJOON/Sliver/S2_11279.java
+++ BACKJOON/Sliver/S2_11279.java
@@ -1,22 +1,22 @@
 package BACKJOON.Sliver;
 
-import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.Scanner;
 
-public class S2_1927 {
+public class S2_11279 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt(); // 연산의 개수
 
-		PriorityQueue<Integer> queue = new PriorityQueue<>(Comparator.reverseOrder());
-		
+		PriorityQueue<Integer> queue = new PriorityQueue<Integer>();
+
 		for (int i = 0; i < n; i++) {
 			int x = sc.nextInt();
 			if (x > 0) {
 				queue.add(x);
+				System.out.println(queue);
 			} else {
 				if (!queue.isEmpty()) {
 					System.out.println(queue.poll());
@@ -25,6 +25,7 @@ public class S2_1927 {
 				}
 			}
 		}
+
 	}
 
 }
