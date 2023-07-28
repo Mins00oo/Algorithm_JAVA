cf36c050dcbd8647abe09f51bfa63b84bf972f7a
SSAFY <SSAFY@DESKTOP-MDJG1NJ>
2023-07-28 17:03:50 +0900

    아직 문제풀이전

--- BACKJOON/GOLD/G4_1922.java
+++ BACKJOON/GOLD/G4_1922.java
@@ -1,10 +1,29 @@
 package BACKJOON.GOLD;
 
-public class G4_1922 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-	public static void main(String[] args) {
+public class G4_1922 {
+	static int n; // 컴퓨터의 수
+	static int m; // 연결할 수 있는 선의 수
+	static StringTokenizer st;
+	static int min = Integer.MAX_VALUE;
+	
+	
+	public static void main(String[] args) throws IOException{
 		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		n = Integer.parseInt(br.readLine());
+		m = Integer.parseInt(br.readLine());
 		
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+		}
 	}
 
 }
