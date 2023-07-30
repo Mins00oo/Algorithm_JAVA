806fb36386325da31c7e720045362bf21b36ca3c
'mins00oo' <'alstn7223@gmail.com'>
2023-07-30 18:59:59 +0900

    FEAT: G4_1922
    처음으로 풀어본 크루스칼 알고리즘 문제! 약간 되게 기본적인 문제 같았다 중요한 건 최소 비용부터 시작하면서
    이어주되 순환 사이클이 만들어지면 안되기 때문에 각 점이 어디까지 연결되어 있는지 확인하고 사이클이
    발생하지 않을때만 연결해주도록!

--- BACKJOON/GOLD/G4_1922.java
+++ BACKJOON/GOLD/G4_1922.java
@@ -1,29 +1,69 @@
 package BACKJOON.GOLD;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class G4_1922 {
 	static int n; // 컴퓨터의 수
 	static int m; // 연결할 수 있는 선의 수
+	static int[][] network; // 각 컴퓨터가 연결된 정보들
 	static StringTokenizer st;
-	static int min = Integer.MAX_VALUE;
-	
-	
-	public static void main(String[] args) throws IOException{
-		// TODO Auto-generated method stub
+	static int costs;
+	static int[] arr; // 어디에 연결되어 있는지 확인하기 위한
+
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		n = Integer.parseInt(br.readLine());
 		m = Integer.parseInt(br.readLine());
-		
-		for (int i = 0; i < m; i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			int c = Integer.parseInt(st.nextToken());
+
+		arr = new int[n + 1];
+		for (int i = 1; i <= n; i++) {
+			arr[i] = i;
 		}
+
+		network = new int[m][3];
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			network[i][0] = Integer.parseInt(st.nextToken());
+			network[i][1] = Integer.parseInt(st.nextToken());
+			network[i][2] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(network, Comparator.comparing(o -> o[2])); // 비용을 기준으로 오름차순 정렬
+
+//        for (int i = 0; i < m; i++) {
+//            for (int j = 0; j < 3; j ++) {
+//                System.out.print(network[i][j]);
+//            }
+//            System.out.println();
+//        }
+
+		for (int[] net : network) {
+			if (union(net[0], net[1])) {
+				costs += net[2];
+			}
+			System.out.println(Arrays.toString(arr));
+		}
+		System.out.println(costs);
 	}
 
+	static int find(int x) { // 연결되었다면 끝지점이 어디인지 탐색
+		if (arr[x] == x) {
+			return x;
+		}
+		return find(arr[x]);
+	}
+
+	static boolean union(int start, int end) {
+		int s = find(start);
+		int e = find(end);
+
+		if (s != e) {
+			arr[e] = s;
+			return true;
+		}
+		return false;
+	}
 }
