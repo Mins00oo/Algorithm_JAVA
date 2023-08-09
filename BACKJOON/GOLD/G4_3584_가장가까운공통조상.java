1bfc9ce8db2aaaed037fa8ce4e6ef112ba5ca0f9
mins00oo <alstn7223@gmail.com>
2023-08-09 12:21:48 +0900

    S1_11286.java
    PriorityQueue에 람다식을 활용해서 우선순위를 정렬

--- BACKJOON/GOLD/G4_3584_가장가까운공통조상.java
+++ BACKJOON/GOLD/G4_3584_가장가까운공통조상.java
@@ -1,7 +1,51 @@
 package BACKJOON.GOLD;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
 public class G4_3584_가장가까운공통조상 {
-	public static void main(String[] args) {
-		
+	static int T, n;
+	static List<List<Integer>> list;
+	static StringTokenizer st;
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		T = Integer.parseInt(br.readLine());
+		for (int t = 1; t <= T; t++) {
+			n = Integer.parseInt(br.readLine());
+			list = new ArrayList<>();
+			for (int i = 0; i <= n; i++) {
+				list.add(new ArrayList<>());
+			}
+			for (int i = 0; i < n - 1; i++) {
+				st = new StringTokenizer(br.readLine());
+				int from = Integer.parseInt(st.nextToken());
+				int to = Integer.parseInt(st.nextToken());
+				list.get(to).add(from);
+			}
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int c1 = Integer.parseInt(st.nextToken());
+			int c2 = Integer.parseInt(st.nextToken());
+			System.out.println(list);
+			sol(c1, c2);
+		}
+		System.out.println(sb);
+	}
+
+	static void sol(int n1, int n2) {
+		// 부모를 찾아갔더니 바로 같은 부모라면 그대로 출력
+		if (list.get(n1).get(0) == list.get(n2).get(0)) {
+			sb.append(list.get(n1).get(0)).append("\n");
+			return;
+		}
+		// 다른 부모일 경우 각자 부모를 계속 찾아가면서 비교
+		while (true) {
+			
+		}
+
 	}
 }
--- BACKJOON/Sliver/S1_11286.java
+++ BACKJOON/Sliver/S1_11286.java
@@ -8,6 +8,7 @@ import java.util.PriorityQueue;
 public class S1_11286 {
 
 	private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
 
 	public static void main(String[] args) throws IOException {
 		int n = Integer.parseInt(br.readLine());
@@ -15,23 +16,27 @@ public class S1_11286 {
 		PriorityQueue<Integer> queue = new PriorityQueue<>((o1, o2) -> {
 			int abs1 = Math.abs(o1);
 			int abs2 = Math.abs(o2);
-
-			if (abs1 == abs2)
-				return o1 > o2 ? 1 : -1;
+			if(abs1 == abs2) {
+				return o1 - o2;
+			}
 			return abs1 - abs2;
 		});
 
 		for (int i = 0; i < n; i++) {
 			int val = Integer.parseInt(br.readLine());
 			if (val == 0) {
-				if (queue.isEmpty())
-					System.out.println("0");
-				else
-					System.out.println(queue.poll());
+				if (queue.isEmpty()) {
+					sb.append(0).append("\n");
+				} else {
+					Integer poll = queue.poll();
+					sb.append(poll).append("\n");
+				}
 			} else {
-				queue.add(val);
+				queue.offer(val);
 			}
 		}
+
+		System.out.println(sb);
 	}
 
 }
