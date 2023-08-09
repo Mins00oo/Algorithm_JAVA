014a44d28ab5250d19c9c079e10782ce222eb25b
mins00oo <alstn7223@gmail.com>
2023-08-09 17:15:59 +0900

    SWEA_1861_정사각형방.java
    visitd 방문 체크를 일일이 계속 해주는거 때문에 시간이 더 오래 걸렸기 때문에
    그 부분을 수정해줬음 어차피 필요가 없는듯

--- SWEA/SWEA_1861_정사각형방.java
+++ SWEA/SWEA_1861_정사각형방.java
@@ -9,7 +9,6 @@ public class SWEA_1861_정사각형방 {
 	static int[] dx = { -1, 1, 0, 0 };
 	static int[] dy = { 0, 0, -1, 1 };
 	static int room, answer, point;
-	static boolean visited[][];
 	static StringBuilder sb = new StringBuilder();
 
 	public static void main(String[] args) throws Exception {
@@ -30,7 +29,6 @@ public class SWEA_1861_정사각형방 {
 			sb.append("#").append(t).append(" ");
 			for (int i = 0; i < n; i++) {
 				for (int j = 0; j < n; j++) {
-					visited = new boolean[n][n];
 					room = 1;
 					dfs(i, j);
 					// 방이 더 큰 경우에는 그냥 그 방의 좌표로 계속 바꾸고
@@ -48,14 +46,13 @@ public class SWEA_1861_정사각형방 {
 	}
 
 	static void dfs(int x, int y) {
-		visited[x][y] = true;
 		for (int i = 0; i < 4; i++) {
 			int nx = dx[i] + x;
 			int ny = dy[i] + y;
 			if (0 > nx || nx >= n || 0 > ny || ny >= n) {
 				continue;
 			}
-			if ((map[nx][ny] - map[x][y] == 1) && !visited[nx][ny]) {
+			if (map[nx][ny] - map[x][y] == 1) {
 				room++;
 				dfs(nx, ny);
 			}
