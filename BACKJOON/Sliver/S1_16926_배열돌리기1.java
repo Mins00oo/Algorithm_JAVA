a5f355cb202502ec91052ade79498c3c0c1560bb
mins00oo <alstn7223@gmail.com>
2023-08-09 16:57:39 +0900

    SWEA_1861_정사각형방.java
    dfs, bfs 둘 다 풀이가 가능한 문제
    출력 부분에서 애를 먹었던 문제,, 생각해보니 세어본 방이 더 클 경우에는
    그 좌표값으로 답을 출력하면 되지만 같을 경우는 따로 생각하는게 더 편함

---
+++ BACKJOON/Sliver/S1_16926_배열돌리기1.java
@@ -0,0 +1,56 @@
+package BACKJOON.Sliver;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class S1_16926_배열돌리기1 {
+	static int n, m, r, arr[][], result[][];
+	static StringTokenizer st;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		r = Integer.parseInt(st.nextToken());
+		arr = new int[n + 1][m + 1];
+		for (int i = 1; i <= n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 1; j <= m; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		result = new int[n + 1][m + 1];
+		for (int i = 0; i < r; i++) {
+			sol();
+		}
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= m; j++) {
+				System.out.print(result[i][j] + " ");
+			}
+			System.out.println();
+		}
+
+	}
+
+	static void sol() {
+		int cycle = n / 2;
+		for (int k = 1; k <= cycle; k++) {
+			for (int i = k; i < n; i++) {
+				result[i + 1][k] = arr[i][k];
+			}
+			for (int i = k; i < m - k + 1; i++) {
+				result[k][i] = arr[k][i + 1];
+			}
+//			for (int i = n - k + 1; i > 0; i--) {
+//				result[i - 1][m - k + 1] = arr[i][m - k + 1];
+//			}
+			for (int i = n - k + 1; i > 0; i--) {
+				result[i - 1][m - k + 1] = arr[i][m - k + 1];
+			}
+		}
+
+	}
+
+}
---
+++ SWEA/SWEA_1861_정사각형방.java
@@ -0,0 +1,65 @@
+package SWEA;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class SWEA_1861_정사각형방 {
+	static int T, n, map[][];
+	static int[] dx = { -1, 1, 0, 0 };
+	static int[] dy = { 0, 0, -1, 1 };
+	static int room, answer, point;
+	static boolean visited[][];
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		T = Integer.parseInt(br.readLine());
+		for (int t = 1; t <= T; t++) {
+			answer = 1;
+			point = Integer.MAX_VALUE;
+			
+			n = Integer.parseInt(br.readLine());
+			map = new int[n][n];
+			for (int i = 0; i < n; i++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				for (int j = 0; j < n; j++) {
+					map[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+			sb.append("#").append(t).append(" ");
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
+					visited = new boolean[n][n];
+					room = 1;
+					dfs(i, j);
+					// 방이 더 큰 경우에는 그냥 그 방의 좌표로 계속 바꾸고
+					if (answer < room) {
+						answer = room;
+						point = map[i][j];
+					} else if (answer == room) { // 방의 개수가 같을 경우에는 작은 좌표값으로 유지시킴
+						point = Math.min(point, map[i][j]);
+					}
+				}
+			}
+			sb.append(point).append(" ").append(answer).append("\n");
+		}
+		System.out.println(sb);
+	}
+
+	static void dfs(int x, int y) {
+		visited[x][y] = true;
+		for (int i = 0; i < 4; i++) {
+			int nx = dx[i] + x;
+			int ny = dy[i] + y;
+			if (0 > nx || nx >= n || 0 > ny || ny >= n) {
+				continue;
+			}
+			if ((map[nx][ny] - map[x][y] == 1) && !visited[nx][ny]) {
+				room++;
+				dfs(nx, ny);
+			}
+
+		}
+	}
+}
