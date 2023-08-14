28ea40cb36173ba33e8e470c1382d983ca36d3bb
mins00oo <alstn7223@gmail.com>
2023-08-14 16:04:03 +0900

    백준_1074_Z.java
    분할정복의 대표적 예시 문제
    시간도 얼마없기 때문에 전체를 하나하나 주어진 좌표까지 체크해볼 수는 없고
    4분면 중에 어디있는지 반으로 잘라서 확인하고 그거를 또 크기를 나누어서
    n이 1이 될 때까지 분할시킴
    이때, 주어진 r과 c값도 어느 사분면에 있는지에 따라서 줄여줘야 함!!!

---
+++ bj/S1/백준_1074_Z.java
@@ -0,0 +1,40 @@
+package bj.S1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 백준_1074_Z {
+	static int N, r, c, cnt;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		r = Integer.parseInt(st.nextToken());
+		c = Integer.parseInt(st.nextToken());
+		solve((int) Math.pow(2, N), r, c);
+		System.out.println(cnt - 1);
+	}
+
+	static void solve(int n, int r, int c) {
+		if (n == 1) {
+			cnt++;
+			return;
+		}
+		// 왼쪽 위에 있을 때
+		if (r < n / 2 && c < n / 2) {
+			solve(n / 2, r, c);
+		} else if (r < n / 2 && c >= n / 2) { // 오른쪽 위에
+			cnt += (n / 2) * (n / 2);
+			System.out.println(cnt);
+			solve(n / 2, r, c - (n / 2));
+		} else if (r >= n / 2 && c < n / 2) {
+			cnt += ((n / 2) * (n / 2)) * 2;
+			solve(n / 2, r - (n / 2), c);
+		} else {
+			cnt += ((n / 2) * (n / 2)) * 3;
+			solve(n / 2, r - (n / 2), c - (n / 2));
+		}
+	}
+}
