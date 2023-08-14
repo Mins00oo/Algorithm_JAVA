8d779bf04d1ff921d360941733d5a1366102627e
mins00oo <alstn7223@gmail.com>
2023-08-14 16:45:08 +0900

    백준_1992_쿼드트리.java
    분할 정복 알고리즘 연습하기 좋은 문제!!
    색종이 그리기 문제랑 되게 비슷하기도 하고 다른 문제들이랑도 푸는 방식은 비슷하다
    먼저 전부 같은 숫자인지 확인하는 메소드를 만들고 다르면 주어진 문제처럼 4분할을 시켜서 계속 쪼갠다

---
+++ bj/S1/백준_1992_쿼드트리.java
@@ -0,0 +1,63 @@
+package bj.S1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/*
+ * 전체를 탐색해서 전부 다 똑같은건지 check해주는데 0이면 0리턴 1이면 1리턴해서 sb에 추가
+ */
+public class 백준_1992_쿼드트리 {
+	static int n;
+	static int[][] map;
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		n = Integer.parseInt(br.readLine());
+		StringTokenizer st;
+		map = new int[n][n];
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine(), "");
+			String s = st.nextToken();
+			for (int j = 0; j < n; j++) {
+				map[i][j] = Integer.parseInt(s.substring(j, j + 1));
+			}
+		}
+		solve(0, 0, n);
+		System.out.println(sb);
+	}
+
+	static void solve(int x, int y, int size) {
+		// 바로 그냥 전부 다 같은 숫자라면 sb에 추가해주고
+		if (check(x, y, size)) {
+			if (map[x][y] == 0) {
+				sb.append(0);
+			} else {
+				sb.append(1);
+			}
+			return;
+		}
+		int newSize = size / 2;
+		// 만약에 다른 숫자가 있다면 4등분해서 다 재귀 돌려야 함
+		sb.append("(");
+		solve(x, y, newSize);
+		solve(x, y + newSize, newSize);
+		solve(x + newSize, y, newSize);
+		solve(x + newSize, y + newSize, newSize);
+		sb.append(")");
+	}
+
+	static boolean check(int x, int y, int size) {
+		// 비교를 해줄 기준점을 잡고
+		int std = map[x][y];
+		for (int i = x; i < x + size; i++) {
+			for (int j = y; j < y + size; j++) {
+				if (map[i][j] != std) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+}
---
+++ bj/S2/백준_1780_종이의개수.java
@@ -0,0 +1,23 @@
+package bj.S2;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/*
+ * 1. 같은 수로 되어있는지 확인 -> 첫 좌표점 가져와서 전체 크기까지 탐색해서 다른점 나오면 false반환하도록
+ * -> 전부 다 같은 수면 그대로 + 1장 해주고
+ * 2. 다른 수로 되어있으면 그대로 거기서 9등분 그대로해서 계속 재귀
+ */
+public class 백준_1780_종이의개수 {
+	static int n;
+	static int[][] map;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+	}
+
+	static boolean check(int x, int y, int size) {
+		return true;
+	}
+}
