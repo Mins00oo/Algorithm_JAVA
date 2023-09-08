98d358ccdfbdc34bfc518a4e475ab4d062fff669
mins00oo <alstn7223@gmail.com>
2023-09-08 09:04:42 +0900

    BOJ_2206

---
+++ bj/G3/BOJ_2206_벽부수고이동하기.java
@@ -0,0 +1,23 @@
+package bj.G3;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_2206_벽부수고이동하기 {
+	static int N, M;
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+	}
+}
+
+
+
+
+
+
