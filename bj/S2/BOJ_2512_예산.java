09739d53c05e0526f18851d0b1cd80e6506c5168
mins00oo <alstn7223@gmail.com>
2023-09-12 17:31:04 +0900

    0912 algo

--- bj/S2/BOJ_2512_예산.java
+++ bj/S2/BOJ_2512_예산.java
@@ -2,37 +2,52 @@ package bj.S2;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class BOJ_2512_예산 {
+	static int N, sum, M;
+	static int[] arr;
+
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-
-		int left = 0, right = -1;
-		int[] arr = new int[n];
+		N = Integer.parseInt(br.readLine());
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < n; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-			right = Math.max(right, arr[i]);
-		}
+		arr = new int[N];
 
-		int m = Integer.parseInt(br.readLine());
-		while (left <= right) {
-			int mid = (left + right) / 2;
-			long budget = 0;
-			for (int i = 0; i < n; i++) {
-				if (arr[i] > mid)
-					budget += mid;
-				else
-					budget += arr[i];
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		M = Integer.parseInt(br.readLine());
+
+		Arrays.sort(arr);
+		sum = 0;
+		
+		for (int a : arr) {
+			sum += a;
+		}
+		
+		int start = arr[0];
+		int end = arr[arr.length - 1];
+		
+		// 예산요청의 합이 M보다 작거나 같으면 최댓값 출력 
+		if (sum <= M) {
+			System.out.println(arr[arr.length - 1]);
+		} else {
+			sum = 0;
+			int mid = (start + end) / 2;
+			
+			for (int ar : arr) {
+				if (ar >= mid) {
+					sum += mid;
+				} else {
+					sum += ar;
+				}
 			}
-			if (budget <= m) {
-				left = mid + 1;
-			} else {
-				right = mid - 1;
+			
+			if (sum <= M) {
+				
 			}
 		}
-		System.out.println(right);
 	}
 }
