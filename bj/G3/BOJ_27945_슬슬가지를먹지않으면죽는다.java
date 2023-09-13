82b21d32f1bacd0eb64cde28eabc1708c9742f44
mins00oo <alstn7223@gmail.com>
2023-09-13 09:03:56 +0900

    BOJ_27945_슬슬가지를먹지않으면죽는다.java

--- bj/G3/BOJ_27945_슬슬가지를먹지않으면죽는다.java
+++ bj/G3/BOJ_27945_슬슬가지를먹지않으면죽는다.java
@@ -9,7 +9,7 @@ import java.util.StringTokenizer;
 
 public class BOJ_27945_슬슬가지를먹지않으면죽는다 {
 	static int N, M;
-	static ArrayList<Info> edge = new ArrayList<>();
+	static ArrayList<Node> edge = new ArrayList<>();
 	static int[] parent;
 	static int D = 1;
 
@@ -31,50 +31,58 @@ public class BOJ_27945_슬슬가지를먹지않으면죽는다 {
 			int u = Integer.parseInt(st.nextToken());
 			int v = Integer.parseInt(st.nextToken());
 			int t = Integer.parseInt(st.nextToken());
-			edge.add(new Info(u, v, t));
+			edge.add(new Node(u, v, t));
 		}
 
 		Collections.sort(edge, (a, b) -> a.t - b.t);
 
-		for (Info e : edge) {
-			if (e.t != D)
+		for (int i = 0; i < M; i++) {
+			Node node = edge.get(i);
+			if (node.t != D) {
 				break;
-			if (findParent(e.u) != findParent(e.v)) {
-				unionGroup(e.u, e.v);
-				D++;
 			}
+			union(node.u, node.v);
 		}
-
 		System.out.println(D);
-	}
-	
-	static int findParent(int x) {
-		if (parent[x] == x) {
-			return x;
-		}
-		return parent[x] = findParent(parent[x]);
+
 	}
 
-	static void unionGroup(int x, int y) {
-		int px = findParent(x);
-		int py = findParent(y);
-
-		if (px < py) {
-			parent[py] = px;
-		} else {
-			parent[px] = py;
-		}
-	}
-	
-	static class Info {
+	static class Node {
 		int u, v, t;
 
-		Info(int u, int v, int t) {
+		public Node(int u, int v, int t) {
 			this.u = u;
 			this.v = v;
 			this.t = t;
 		}
+
 	}
+
+	static int find(int x) {
+		if (parent[x] == x) {
+			return x;
+		}
+		return parent[x] = find(parent[x]);
+	}
+
+	static void union(int a, int b) {
+		int a_parent = find(a);
+		int b_parent = find(b);
+
+		if (a_parent != b_parent) {
+			if (a_parent < b_parent) {
+				parent[b_parent] = a_parent;
+				D++;
+			} else {
+				parent[a_parent] = b_parent;
+				D++;
+			}
+		}
+
+	}
+
 }
+
+
 
 
