96ea24eba3f3b51bc45fdef1881942400a7ad235
'mins00oo' <'alstn7223@gmail.com'>
2023-10-08 16:38:43 +0900

    PGS_캐시.java

--- programmers/Lv2/PGS_숫자의표현.java
+++ programmers/Lv2/PGS_숫자의표현.java
@@ -1,9 +1,28 @@
 package programmers.Lv2;
 
+/*
+ */
 public class PGS_숫자의표현 {
 	
     public int solution(int n) {
         int answer = 0;
+        
+        for (int i = 1; i <= n; i++) {
+			int sum = 0;
+			for (int j = i; j <= n; j++) {
+				sum += j;
+				
+				if (sum == n) {
+					answer++;
+				} else if (sum > n) {
+					break;
+				}
+			}
+		}
         return answer;
     }
+    
+    public static void main(String[] args) {
+		System.out.println(new PGS_숫자의표현().solution(15));
+	}
 }
