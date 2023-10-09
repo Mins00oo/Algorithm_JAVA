8da808a9675a3b2829ac78220d00f56e1d7d7d5b
'mins00oo' <'alstn7223@gmail.com'>
2023-10-10 06:11:51 +0900

    PGS_영어끝말잇기.java

--- programmers/Lv2/PGS_영어끝말잇기.java
+++ programmers/Lv2/PGS_영어끝말잇기.java
@@ -3,7 +3,7 @@ package programmers.Lv2;
 public class PGS_영어끝말잇기 {
 	public int[] solution(int n, String[] words) {
 		int[] answer = {};
-
+		
 		// [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
 		System.out.println("Hello Java");
 
