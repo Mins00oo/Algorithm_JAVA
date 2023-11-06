24bbbc0e0f1b9a6a7e3aef063a089659687be48e
mins00oo <alstn7223@gmail.com>
2023-11-06 17:46:42 +0900

    alog

---
+++ bj/S1/BOJ_1446_지름길.java
@@ -0,0 +1,18 @@
+package bj.S1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_1446_지름길 {
+	static int N, D;
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		D = Integer.parseInt(st.nextToken());
+		
+	}
+}
