71b1a5af7f3afedbbf1927ea0af2ad31a821be2e
mins00oo <alstn7223@gmail.com>
2024-01-03 17:43:42 +0900

    BOJ 20437 문자열 게임 2
    아직 문제 완전히는 해결 못해서 중간에 커밋

---
+++ bj/G5/BOJ_20437_문자열_게임2.java
@@ -0,0 +1,20 @@
+package bj.G5;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BOJ_20437_문자열_게임2 {
+    static int T;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        T = br.read();
+
+        for (int t = 0; t < T; t++) {
+
+        }
+
+
+    }
+}
